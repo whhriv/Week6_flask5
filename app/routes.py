7c39b37cd69018a65c1604d564a9e78bd038fc4c
William Reeder <whhriv@gmail.com>
2023-10-23 19:49:41 -0700

    The server can't find the links, but it goes it index.html on default, but /index.html isn't found

---
+++ app/routes.py
@@ -0,0 +1,13 @@
+from app import app
+from flask import render_template
+
+
+@app.route('/')
+def index():
+    name = 'Humans'
+    skis= ['POWABUNGA', 'Jeffery 110', 'Mango100', 'shredditor', 'domain']
+    return render_template('index.html')
+
+@app.route('/faves')
+def faves():
+    return render_template('faves.html')
